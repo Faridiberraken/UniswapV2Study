4e3b9839a1c876dae16528f40c4257c9fd54b95c
Faridiberraken <farid.iberraken@gmail.com>
2024-08-04 11:12:01 +0200

    second commit

---
+++ test/UniswapV2SwapTest.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract UniswapV2SwapTest is Test {
+    function setUp() public {}
+}
